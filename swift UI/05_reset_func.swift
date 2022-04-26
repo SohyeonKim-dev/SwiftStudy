f285debbbb27b028d5430d3dcebcee35fa634fb4
SoHyeon Kim <happythgus89@gmail.com>
2022-04-26 21:09:48 +0900

    reset

---
+++ swift UI/05_reset_func.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 김소현 on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb.png"), for: .normal)
+        
+        reset()
+    }
+
+    @IBAction func sliderValueChanged (_ sender : UISlider){
+        // 대소문자, 띄어쓰기 매우 중요!
+        print(sender.value)
+        // slider 오류 발생(cmt + R -> 안 뜸), app icon 때문인가?
+        // IBAction : interface와 연결되는 함수라는 의미!
+    }
+    
+    @IBAction func touchUpHitButton (_ sender : UIButton){
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton (_ sender : UIButton){
+        reset()
+    }
+    
+    func reset() {
+        print("reset")
+    }
+}
