9b5aa428fba65200716569753b6c84833b4909a9
SoHyeon Kim <happythgus89@gmail.com>
2022-04-26 18:49:36 +0900

    first!

---
+++ swiftUI/01_slider.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 김소현 on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sliderValueChanged (_ sender : UISlider){
+        // 대소문자, 띄어쓰기 매우 중요!
+        print(sender.value)
+    }
+
+}
+
