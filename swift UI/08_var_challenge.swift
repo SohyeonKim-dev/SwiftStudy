ec85d41b30faf9235a197d683fcfa818b7c1dfd2
SoHyeon Kim <happythgus89@gmail.com>
2022-04-27 10:27:50 +0900

    challenge

---
+++ swift UI/08_var_challenge.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 김소현 on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomValue : Int = 0
+    var tryCount : Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel : UILabel!
+    @IBOutlet weak var sliderValueLabel : UILabel!
+    @IBOutlet weak var minimumValueLabel : UILabel!
+    @IBOutlet weak var maximumValueLabel : UILabel!
+    // 선언한 변수의 오타를 고칠때는 꼭 우클릭, refactor, rename 과정으로 수정하기!! 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb.png3"), for: .normal)
+        
+        reset()
+    }
+
+    @IBAction func sliderValueChanged (_ sender : UISlider){
+        // 대소문자, 띄어쓰기 매우 중요!
+        print(sender.value) // view controller error debugging clear! :)
+    }
+    
+    @IBAction func touchUpHitButton (_ sender : UIButton){
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton (_ sender : UIButton){
+        reset()
+    }
+    
+    func reset() {
+        print("reset")
+    }
+}
