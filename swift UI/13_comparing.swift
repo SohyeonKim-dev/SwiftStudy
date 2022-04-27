a3e7c457783fc72e9f766c3d77d08328856b8283
SoHyeon Kim <happythgus89@gmail.com>
2022-04-27 11:09:15 +0900

    comparing hit value

---
+++ swift UI/13_comparing.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 김소현 on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomValue : Int = 0
+    var tryCount : Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel : UILabel!
+    @IBOutlet weak var sliderValueLabel : UILabel!
+    @IBOutlet weak var minimumValueLabel : UILabel!
+    @IBOutlet weak var maximumValueLabel : UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb.png3"), for: .normal)
+        
+        reset()
+    }
+
+    @IBAction func sliderValueChanged (_ sender : UISlider){
+        print(sender.value)
+        let integerValue : Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    @IBAction func touchUpHitButton (_ sender : UIButton){
+        print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            print("You Hit!")
+        }
+    }
+    
+    @IBAction func touchUpResetButton (_ sender : UIButton){
+        reset()
+    }
+    
+    func reset() {
+        print("reset")
+        randomValue = Int.random(in: 0...30)
+        // print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+    }
+}
