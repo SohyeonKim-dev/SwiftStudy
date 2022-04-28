4b1db2b004ceb7ee7b676d329149b4a31a445df3
SoHyeon Kim <happythgus89@gmail.com>
2022-04-28 11:52:04 +0900

    modal

---
+++ swift UI/17_dismiss_button.swift
@@ -0,0 +1,19 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 김소현 on 2022/04/27.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton (_ sender: UIButton){
+        dismiss(animated: true,
+                completion: nil)
+    }
+}
+
+
+// modal back button
