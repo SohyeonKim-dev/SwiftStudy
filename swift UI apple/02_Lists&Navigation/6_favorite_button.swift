fb8904b3ed299b751365abb9bae11569015c5083
SoHyeon Kim <happythgus89@gmail.com>
2022-05-01 16:31:10 +0900

    FavoriteButton

---
+++ swift UI apple/02_Lists&Navigation/6_favorite_button.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  swift UI colli's cat music app
+//
+//  Created by 김소현 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
