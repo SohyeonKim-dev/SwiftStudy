11f377bc851f82b4439ed9b1f6cb4f5814c2a243
SoHyeon Kim <happythgus89@gmail.com>
2022-05-01 16:19:53 +0900

    Add toggle

---
+++ swift UI apple/02_Lists&Navigation/5_favorite_toggle.swift
@@ -0,0 +1,42 @@
+//
+//  LandmarkList.swift
+//  swift UI colli's cat music app
+//
+//  Created by 김소현 on 2022/05/01.
+//
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
