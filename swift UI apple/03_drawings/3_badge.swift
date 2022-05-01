9476913bc1a9275dbb65595f6fff1dc675b8b822
SoHyeon Kim <happythgus89@gmail.com>
2022-05-01 22:04:39 +0900

    badge

---
+++ swift UI apple/03_drawings/3_badge.swift
@@ -0,0 +1,38 @@
+//
+//  Badge.swift
+//  swift UI colli's cat music app
+//
+//  Created by 김소현 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
