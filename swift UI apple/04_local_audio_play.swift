c2a33beaa64b004a4adab3778a2e92292446df5b
SoHyeon Kim <happythgus89@gmail.com>
2022-05-03 12:34:56 +0900

    local audio

---
+++ swift UI apple/04_local_audio_play.swift
@@ -0,0 +1,24 @@
+    //
+    //    var soundEffect: AVAudioPlayer?
+    //
+    //    init() {
+    //        playAudio()
+    //    }
+    //
+    //    mutating func playAudio() {
+    //
+    //        let url = Bundle.main.url(forResource: "startBGM", withExtension: "m4a")
+    //
+    //        if let url = url{
+    //            do {
+    //                self.soundEffect = try AVAudioPlayer(contentsOf: url)
+    //                guard let sound = soundEffect else { return }
+    //
+    //                //sound.prepareToPlay()
+    //                sound.play()
+    //            } catch let error {
+    //                print(error.localizedDescription)
+    //            }
+    //        }
+    //    }
+    //
